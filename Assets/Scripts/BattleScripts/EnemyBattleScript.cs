93cdbbd7a0f06179cb858101468ee03b1c03dda0
JVRicci <54457696+JVRicci@users.noreply.github.com>
2025-03-15 04:48:07 -0300

    Criação de prefab enemy em batalha

---
+++ Assets/Scripts/BattleScripts/EnemyBattleScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+
+public class EnemyBattleScript : MonoBehaviour
+{
+    public float attackWaitTime = .5f;
+    public float moveSpeed = 25f;
+    public TMP_Text BattleInput; 
+    public Transform enemyTransform ;
+    public Vector2 enemyStartPosition;     // Guarda a posição inicial do inimigo
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyTransform = GetComponent<Transform>();
+        enemyStartPosition = enemyTransform.position; // Guarda a posição inicial do player
+    }
+
+    // public void EnemyAction(){
+    //     StartCoroutine(EnemyAttack());
+    // }
+    
+    // public IEnumerator EnemyAttack()
+    // {
+    //     yield return new WaitForSeconds(attackWaitTime + .5f);
+    //     BattleInput.text += "\nO inimigo atacou!";
+
+    //     GameObject playerObject = GameBattleControllerScript.instance.playerObject;
+    //     Transform playerTransform = playerObject.GetComponent<Transform>();
+
+    //     // Simula o ataque (adiciona um delay antes de voltar)
+    //     yield return StartCoroutine(MoveToPosition(enemyTransform, new Vector2(playerTransform.position.x + 2, playerTransform.position.y)));
+
+    //     yield return new WaitForSeconds(attackWaitTime);
+
+    //     yield return StartCoroutine(MoveToPosition(enemyTransform, enemyStartPosition));
+    //     // Espera um frame para garantir que a interface seja atualizada antes de continuar
+    //     yield return new WaitForSeconds(1f);
+        
+    //     GameBattleControllerScript.instance.PlayerTurn();
+    // }
+
+    // IEnumerator MoveToPosition(Transform obj, Vector2 target)
+    // {
+    //     while ((Vector2)obj.position != target)
+    //     {
+    //         obj.position = Vector2.MoveTowards(obj.position, target, moveSpeed * Time.deltaTime);
+
+    //         yield return null; // Espera o próximo frame
+
+    //     }
+    // }
+}
--- Assets/Scripts/BattleScripts/GameBattleControllerScript.cs
+++ Assets/Scripts/BattleScripts/GameBattleControllerScript.cs
@@ -81,4 +81,5 @@ public class GameBattleControllerScript : MonoBehaviour
         }
     }
 
+    
 }
